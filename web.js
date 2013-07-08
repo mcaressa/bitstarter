ce4ce48330627da95057fada30a4768116962d7a
mcaressa <marcoc@marcoc-ubuntu.(none)>
2013-07-08 22:59:41 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var getFileContent = function(fileName) {
+  return fs.readFileSync('index.html');
+};
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+  response.send(getFileContent);
 });
 
 var port = process.env.PORT || 5000;
