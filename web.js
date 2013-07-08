84b63bb1dfe7027375c815fbdd5a00cf7c1f25e7
mcaressa <marcoc@marcoc-ubuntu.(none)>
2013-07-08 23:15:27 +0200

    web.js

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 var getFileContent = function(fileName) {
   return   fs.readFileSync(fileName,'utf-8');
 };
